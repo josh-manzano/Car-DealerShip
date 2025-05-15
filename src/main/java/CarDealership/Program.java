0bc320d700021195970d893fec5b79789a338d41
Josh M <manzojosh27@gmail.com>
2025-05-15 14:02:49 -0700

    dealership pt2

---
+++ src/main/java/CarDealership/Program.java
@@ -0,0 +1,14 @@
+package CarDealership;
+
+public class Program {
+    public static void main(String[] args) {
+
+        // creating an object to get access to a Class methods or information
+
+        UserInterface ui = new UserInterface();
+        // When you have a static you can access by Class name (UserInterface)
+        // When you don't have static you need to create an object (UserInterface- object) to access methods or information
+        ui.displayMenu();
+    }
+
+}
